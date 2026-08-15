d402041df6f8e63d75db7d456fbf6d030e223828
Hajinwoo28 <reynaldjakemalabanan328@gmail.com>
2026-08-15 20:02:03 +0800

    Apply flutter analyze fixes

--- Unknown folder/texttospeech.py
+++ Unknown folder/texttospeech.py
@@ -8,7 +8,7 @@ from gtts import gTTS
 #    :param lang: The language for the speech (default is English).
 #    :param filename: The name of the output audio file (default is 'output.mp3').
 #    """
-text = "putang ina mo!"
+text = "fuck you tonee!"
 
 tts = gTTS(text=text, lang='tl')#tl(tagalog), en(english), es(spanish), fr(french), de(german), it(italian), ja(japanese), ko(korean), zh-cn(chinese), id(indonesian), hi(hindi), ar(arabic), ru(russian), pt(portuguese), vi(vietnamese), th(thai), tr(turkish), pl(polish), nl(dutch), sv(swedish), fi(finnish), no(norwegian), da(danish), cs(czech), el(greek), hu(hungarian), ro(romanian), sk(slovak), bg(bulgarian)
 tts.save('output.mp3')#filename
